5b285a1ea6ab288595c01f0f927fae62a3e9c901
matthewa313 <mwanderson04@gmail.com>
2018-07-28 16:44:17 -0600

    Document functions.php
    Improved comments and documentation of functions.php

--- facemashCloneFiles/functions.php
+++ facemashCloneFiles/functions.php
@@ -1,16 +1,19 @@
 <?php
 
-// Calculate the expected % outcome
+// complete the following via the Elo algorithm
+// https://en.wikipedia.org/wiki/Elo_rating_system
+
+// calculate the expected outcome (out of 1)
 function expected($Rb, $Ra) {
 	return 1/(1 + pow(10, ($Rb-$Ra)/400));
 }
 
-// Calculate the new winnner score
+// calculate the Elo of new winner
 function win($score, $expected, $k = 8) {
 	return $score + $k * (1-$expected);
 }
 
-// Calculate the new loser score
+// calculate the Elo of new loser
 function loss($score, $expected, $k = 8) {
 	return $score + $k * (0-$expected);
 }
